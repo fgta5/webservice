c7544487967e11e2103c9f4f52d2b78cc8e1b735
agungdhewe <agung_dhewe@yahoo.com>
2024-05-07 02:59:36 +0700

    add module in subpage

--- src/jslibs/web-subpage-1.mjs
+++ src/jslibs/web-subpage-1.mjs
@@ -5,14 +5,37 @@ export function Construct() {
 	self.Pages = {}
 }
 
-export function AddSubpage(el, isdefault) {
+export function getPage(id) {
+	return self.Pages[id]
+}
+
+export function AddSubpage(el, module, isdefault) {
+	el.module = module
+	el.Show = ()=> {
+		self.currentSubPage.classList.add('hidden');
+		
+		el.classList.remove('hidden');
+		self.currentSubPage = el
+	}
 	self.Pages[el.id] = el
 	if (isdefault) {
-		self.currentSubPage = el
+		self.currentSubPage = self.Pages[el.id]
 	}
 }
 
-export function Show(el) {
+export function Show(obj) {
+	var el;
+	if (typeof obj === 'string') {
+		el = document.getElementById(obj)
+	} else {
+		el = obj;
+	}
+
+	if (obj==null) {
+		console.error('sub page yang akan ditampilkan tidak ditemukan')
+		return;
+	}
+
 	// fading currentSubPage
 	self.currentSubPage.classList.add('hidden');
 
